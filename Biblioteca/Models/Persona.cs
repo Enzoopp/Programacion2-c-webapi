12d7629387a8df1ff0ce0e51df7266146eb676fa
Enzo Pitana <enzopitana@gmail.com>
2025-09-08 11:00:27 -0300

    Update Persona.cs

--- Biblioteca/Models/Persona.cs
+++ Biblioteca/Models/Persona.cs
@@ -1,11 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Biblioteca.Models
 {
     public class Persona
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es requerido")]
         public string Apellido { get; set; }
+
         public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "El DNI es requerido")]
+        [StringLength(8, MinimumLength = 7, ErrorMessage = "El DNI debe tener entre 7 y 8 caracteres")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "El DNI solo puede contener números")]
         public string Dni { get; set; }
     }
 }
+
