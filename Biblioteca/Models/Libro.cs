0174f41d67f9805daf93b10e93c3df0302d440de
Enzo Pitana <enzopitana@gmail.com>
2025-09-08 11:00:25 -0300

    Create Libro.cs

---
+++ Biblioteca/Models/Libro.cs
@@ -0,0 +1,15 @@
+namespace Biblioteca.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public int IdAutor { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Genero { get; set; }
+        public string ISBN { get; set; }
+        public int Fecha_Publicacion { get; set; }
+        public int Fecha_Edicion { get; set; }
+        public string Edicion { get; set; }
+    }
+}
