648065cc3ab764ad92df7efeccf476590550e03b
Enzo Pitana <enzopitana@gmail.com>
2025-09-15 12:03:01 -0300

    Update Program.cs

--- Biblioteca/Program.cs
+++ Biblioteca/Program.cs
@@ -1,7 +1,15 @@
 using Biblioteca.interfaces;
 using Biblioteca.Services;
+using EFCorePersonaApi.Data;
+using EFCorePersonaApi.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+// 1. Añadir cadena de conexión y DbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 
 // Add services to the container.
 
@@ -14,6 +22,9 @@ builder.Services.AddSingleton<IFileStorageService, FileStorageService>();
 builder.Services.AddSingleton<IPersonaService, PersonaFileServices>();
 builder.Services.AddSingleton<IAutorService, AutorFileService>();
 builder.Services.AddSingleton<ILibroService, LibroFileService>();
+builder.Services.Addscoped<IPersonaService, PersonaDbServices>();
+builder.Services.Addscoped<IAutorService, AutorDbService>();
+builder.Services.AddScoped<ILibroService, LibroDbService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
