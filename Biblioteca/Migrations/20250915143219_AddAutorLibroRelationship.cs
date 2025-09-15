127e0cc22e146aefd15a49c14bf2cf5661da9b62
Enzo Pitana <enzopitana@gmail.com>
2025-09-15 12:02:47 -0300

    Create 20250915143219_AddAutorLibroRelationship.cs

---
+++ Biblioteca/Migrations/20250915143219_AddAutorLibroRelationship.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Biblioteca.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAutorLibroRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Libros_IdAutor",
+                table: "Libros",
+                column: "IdAutor");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Libros_Autores_IdAutor",
+                table: "Libros",
+                column: "IdAutor",
+                principalTable: "Autores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Libros_Autores_IdAutor",
+                table: "Libros");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Libros_IdAutor",
+                table: "Libros");
+        }
+    }
+}
