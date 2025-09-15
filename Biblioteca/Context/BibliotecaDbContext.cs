e55680ca40ea7dabbf2ae7d5638543469a459e72
Enzo Pitana <enzopitana@gmail.com>
2025-09-15 12:02:41 -0300

    Update BibliotecaDbContext.cs

---
+++ Biblioteca/Context/BibliotecaDbContext.cs
@@ -0,0 +1,24 @@
+using Biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Context
+{
+    public class BibliotecaDbContext : DbContext
+    {
+        public BibliotecaDbContext(DbContextOptions<BibliotecaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Libro>()
+                .HasOne(l => l.Autor)
+                .WithMany(a => a.Libros)
+                .HasForeignKey(l => l.IdAutor);
+        }
+    }
+}
