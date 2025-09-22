5f656206a60ccac4ddfe36bed2d5d3c3a68bd574
Enzo Pitana <enzopitana@gmail.com>
2025-09-22 01:04:51 -0300

    Update Autor.cs

--- Biblioteca/Models/Autor.cs
+++ Biblioteca/Models/Autor.cs
@@ -1,12 +1,18 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Biblioteca.Models
 {
     public class Autor
     {
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "El nombre es requerido")]
         public string Nombre { get; set; }
+        
+        [Required(ErrorMessage = "La nacionalidad es requerida")]
         public string Nacionalidad { get; set; }
+        
         public string Descripcion { get; set; }
         public List<Libro> Libros { get; set; }
     }
