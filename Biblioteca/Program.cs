fe65af6cf2910ed82e0a7e36109a6ee2c5eb6d0c
Enzo Pitana <enzopitana@gmail.com>
2025-09-22 01:04:54 -0300

    Update Program.cs

--- Biblioteca/Program.cs
+++ Biblioteca/Program.cs
@@ -1,13 +1,12 @@
 using Biblioteca.interfaces;
 using Biblioteca.Services;
-using EFCorePersonaApi.Data;
-using EFCorePersonaApi.Services;
+using Biblioteca.Context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 // 1. Añadir cadena de conexión y DbContext
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<BibliotecaDbContext>(options =>
     options.UseSqlServer(connectionString));
 
 
@@ -19,12 +18,20 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IFileStorageService, FileStorageService>();
-builder.Services.AddSingleton<IPersonaService, PersonaFileServices>();
-builder.Services.AddSingleton<IAutorService, AutorFileService>();
-builder.Services.AddSingleton<ILibroService, LibroFileService>();
-builder.Services.Addscoped<IPersonaService, PersonaDbServices>();
-builder.Services.Addscoped<IAutorService, AutorDbService>();
-builder.Services.AddScoped<ILibroService, LibroDbService>();
+
+// Elegir UNA estrategia por entorno:
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddScoped<IPersonaService, PersonaFileServices>();
+    builder.Services.AddScoped<IAutorService, AutorFileService>();
+    builder.Services.AddScoped<ILibroService, LibroFileService>();
+}
+else
+{
+    builder.Services.AddScoped<IPersonaService, PersonaDbServices>();
+    builder.Services.AddScoped<IAutorService, AutorDbService>();
+    // builder.Services.AddScoped<ILibroService, LibroDbService>(); // Crear este servicio
+}
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
