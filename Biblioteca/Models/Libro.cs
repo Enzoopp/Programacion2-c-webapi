21df1764bb551887cdec416e53e36df94bd6b56a
Enzo Pitana <enzopitana@gmail.com>
2025-09-22 17:51:44 -0300

    Update Libro.cs

--- Biblioteca/Models/Libro.cs
+++ Biblioteca/Models/Libro.cs
@@ -8,10 +8,9 @@ namespace Biblioteca.Models
         public string Descripcion { get; set; }
         public string Genero { get; set; }
         public string ISBN { get; set; }
-        public int Fecha_Publicacion { get; set; }
-        public int Fecha_Edicion { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public DateTime FechaEdicion { get; set; }
         public string Edicion { get; set; }
-
         public Autor Autor { get; set; }
     }
 }
