418cd129a894749fd5ab3e9cbd8dc34c3141a56f
Enzo Pitana <enzopitana@gmail.com>
2025-10-06 11:34:52 -0300

    Update LibroMemService.cs

--- Biblioteca/Service/LibroMemService.cs
+++ Biblioteca/Service/LibroMemService.cs
@@ -9,9 +9,9 @@ namespace Biblioteca.Services
     {
         private static List<Libro> _libros = new List<Libro>
         {
-            new Libro { Id = 1, IdAutor = 1, Titulo = "Cien Años de Soledad", Descripcion = "La novela narra la historia de la familia Buendía a lo largo de siete generaciones en el pueblo ficticio de Macondo.", Genero = "Realismo Mágico", ISBN = "978-0307474728", Fecha_Publicacion = 1967, Fecha_Edicion = 2003, Edicion = "Edición Conmemorativa" },
-            new Libro { Id = 2, IdAutor = 2, Titulo = "Rayuela", Descripcion = "La novela es conocida por su estructura, que permite al lector elegir entre leer la novela de forma lineal o seguir un orden de capítulos no secuencial.", Genero = "Novela", ISBN = "978-8437604034", Fecha_Publicacion = 1963, Fecha_Edicion = 2019, Edicion = "Edición Cátedra" },
-            new Libro { Id = 3, IdAutor = 3, Titulo = "La Ciudad y los Perros", Descripcion = "La novela narra las vivencias de un grupo de jóvenes internos en un colegio militar en Lima, Perú.", Genero = "Novela", ISBN = "978-8466333868", Fecha_Publicacion = 1963, Fecha_Edicion = 2016, Edicion = "Edición Alfaguara" }
+            new Libro { Id = 1, IdAutor = 1, Titulo = "Cien Años de Soledad", Descripcion = "La novela narra la historia de la familia Buendía a lo largo de siete generaciones en el pueblo ficticio de Macondo.", Genero = "Realismo Mágico", ISBN = "978-0307474728", FechaPublicacion = new DateTime(1967, 1, 1), FechaEdicion = new DateTime(2003, 1, 1), Edicion = "Edición Conmemorativa" },
+            new Libro { Id = 2, IdAutor = 2, Titulo = "Rayuela", Descripcion = "La novela es conocida por su estructura, que permite al lector elegir entre leer la novela de forma lineal o seguir un orden de capítulos no secuencial.", Genero = "Novela", ISBN = "978-8437604034", FechaPublicacion = new DateTime(1963, 1, 1), FechaEdicion = new DateTime(2019, 1, 1), Edicion = "Edición Cátedra" },
+            new Libro { Id = 3, IdAutor = 3, Titulo = "La Ciudad y los Perros", Descripcion = "La novela narra las vivencias de un grupo de jóvenes internos en un colegio militar en Lima, Perú.", Genero = "Novela", ISBN = "978-8466333868", FechaPublicacion = new DateTime(1963, 1, 1), FechaEdicion = new DateTime(2016, 1, 1), Edicion = "Edición Alfaguara" }
         };
 
         public List<Libro> GetAll()
@@ -42,8 +42,8 @@ namespace Biblioteca.Services
                 existingLibro.Descripcion = libro.Descripcion;
                 existingLibro.Genero = libro.Genero;
                 existingLibro.ISBN = libro.ISBN;
-                existingLibro.Fecha_Publicacion = libro.Fecha_Publicacion;
-                existingLibro.Fecha_Edicion = libro.Fecha_Edicion;
+                existingLibro.FechaPublicacion = libro.FechaPublicacion;
+                existingLibro.FechaEdicion = libro.FechaEdicion;
                 existingLibro.Edicion = libro.Edicion;
             }
         }
