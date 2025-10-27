44961610154c881a66e3103b4b15892dabcd51a8
Enzo Pitana <enzopitana@gmail.com>
2025-10-27 15:23:49 -0300

    Create AuthOptions.cs

---
+++ BankLink/Models/AuthOptions.cs
@@ -0,0 +1,10 @@
+namespace BankLink.Models
+{
+    public class AuthOptions
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Key { get; set; }
+        public int ExpMinutes { get; set; }
+    }
+}
