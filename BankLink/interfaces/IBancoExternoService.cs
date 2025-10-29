41471ca302d02f866da105b39799079347087475
Enzo Pitana <enzopitana@gmail.com>
2025-10-29 10:45:38 -0300

    Update IBancoExternoService.cs

--- BankLink/interfaces/IBancoExternoService.cs
+++ BankLink/interfaces/IBancoExternoService.cs
@@ -4,18 +4,11 @@ namespace BankLink.interfaces
 {
     public interface IBancoExternoService
     {
-        Task<List<BancoExterno>> GetAllAsync();
-        Task<BancoExterno?> GetByIdAsync(int id);
-        Task<BancoExterno?> GetByCodigoIdentificacionAsync(string codigo);
-        Task<BancoExterno> CreateAsync(BancoExterno banco);
-        Task UpdateAsync(int id, BancoExterno banco);
-        Task DeleteAsync(int id);
-        Task<bool> ExistsAsync(int id);
-        Task<bool> ExistsByCodigoAsync(string codigo);
-        Task CambiarEstadoAsync(int id, bool activo);
-        
-        // Métodos para integración con APIs externas
-        Task<bool> ValidarConexionAsync(int bancoId);
-        Task<bool> EnviarTransferenciaAsync(int bancoId, string numeroCuentaDestino, decimal monto, string descripcion, string referenciaInterna);
+        List<BancoExterno> GetAll();
+        BancoExterno GetById(int id);
+        BancoExterno GetByCodigoIdentificacion(string codigo);
+        BancoExterno Create(BancoExterno banco);
+        void Update(int id, BancoExterno banco);
+        void Delete(int id);
     }
 }
