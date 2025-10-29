249a26afa996b53a630484b84cf8d4d957078203
Enzo Pitana <enzopitana@gmail.com>
2025-10-29 10:45:35 -0300

    Update ICuentaService.cs

--- BankLink/interfaces/ICuentaService.cs
+++ BankLink/interfaces/ICuentaService.cs
@@ -4,18 +4,13 @@ namespace BankLink.interfaces
 {
     public interface ICuentaService
     {
-        Task<List<Cuenta>> GetAllAsync();
-        Task<Cuenta?> GetByIdAsync(int id);
-        Task<Cuenta?> GetByNumeroCuentaAsync(string numeroCuenta);
-        Task<List<Cuenta>> GetByClienteIdAsync(int clienteId);
-        Task<Cuenta> CreateAsync(Cuenta cuenta);
-        Task UpdateAsync(int id, Cuenta cuenta);
-        Task DeleteAsync(int id);
-        Task<bool> ExistsAsync(int id);
-        Task<bool> ExistsByNumeroCuentaAsync(string numeroCuenta);
-        Task<decimal> GetSaldoAsync(int cuentaId);
-        Task ActualizarSaldoAsync(int cuentaId, decimal nuevoSaldo);
-        Task<bool> TieneSaldoSuficienteAsync(int cuentaId, decimal monto);
-        Task CambiarEstadoAsync(int cuentaId, EstadoCuenta nuevoEstado);
+        List<Cuenta> GetAll();
+        Cuenta GetById(int id);
+        Cuenta GetByNumeroCuenta(string numeroCuenta);
+        List<Cuenta> GetByClienteId(int clienteId);
+        Cuenta Create(Cuenta cuenta);
+        void Update(int id, Cuenta cuenta);
+        void Delete(int id);
+        void ActualizarSaldo(int id, decimal nuevoSaldo);
     }
 }
