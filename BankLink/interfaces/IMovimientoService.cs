2aa4e9c52a83878462bef8ea3b72648e6fed0492
Enzo Pitana <enzopitana@gmail.com>
2025-10-29 10:45:32 -0300

    Update IMovimientoService.cs

--- BankLink/interfaces/IMovimientoService.cs
+++ BankLink/interfaces/IMovimientoService.cs
@@ -4,18 +4,10 @@ namespace BankLink.interfaces
 {
     public interface IMovimientoService
     {
-        Task<List<Movimiento>> GetAllAsync();
-        Task<Movimiento?> GetByIdAsync(int id);
-        Task<List<Movimiento>> GetByCuentaIdAsync(int cuentaId);
-        Task<List<Movimiento>> GetByTransferenciaIdAsync(int transferenciaId);
-        Task<Movimiento> CreateAsync(Movimiento movimiento);
-        Task<List<Movimiento>> GetHistorialCuentaAsync(int cuentaId, DateTime? fechaDesde = null, DateTime? fechaHasta = null);
-        Task<decimal> CalcularSaldoCuentaAsync(int cuentaId);
-        
-        // Métodos específicos para operaciones bancarias
-        Task<Movimiento> RegistrarDepositoAsync(int cuentaId, decimal monto, string descripcion);
-        Task<Movimiento> RegistrarRetiroAsync(int cuentaId, decimal monto, string descripcion);
-        Task<Movimiento> RegistrarTransferenciaEnviadaAsync(int cuentaId, decimal monto, string descripcion, int? transferenciaId = null);
-        Task<Movimiento> RegistrarTransferenciaRecibidaAsync(int cuentaId, decimal monto, string descripcion, int? transferenciaId = null);
+        List<Movimiento> GetAll();
+        Movimiento GetById(int id);
+        List<Movimiento> GetByCuentaId(int cuentaId);
+        Movimiento Create(Movimiento movimiento);
+        void Delete(int id);
     }
 }
