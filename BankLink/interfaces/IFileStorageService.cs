2be09613b701c158dfa0da13c055f0fef7435154
Enzo Pitana <enzopitana@gmail.com>
2025-11-09 20:51:30 -0300

    Delete IFileStorageService.cs

--- BankLink/interfaces/IFileStorageService.cs
+++
@@ -1,8 +0,0 @@
-namespace BankLink.interfaces
-{
-    public interface IFileStorageService
-    {
-        string Read(string filePath);
-        void Write(string filePath, string fileContent);
-    }
-}
